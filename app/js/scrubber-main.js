0576f4a812516d5c42697c9220dcefe9993742ec
Raymond Zhou <somecrazyidiot@gmail.com>
2014-08-07 02:48:39 -0400

    Add drawScrubber() function.
    Draws a window into the backing store canvas.

--- app/js/scrubber-main.js
+++ app/js/scrubber-main.js
@@ -1,5 +1,10 @@
+/*global requestAnimationFrame*/
 (function() {
   'use strict';
+
+  var config = {
+    length: 64
+  };
 
   /**
    * There are three canvases:
@@ -19,6 +24,10 @@
   canvas.backingStore.height = 512;
   document.body.appendChild( canvas.backingStore );
 
+  canvas.scrubber.width  = 64;
+  canvas.scrubber.height = 256;
+  document.body.appendChild( canvas.scrubber );
+
   function connect() {
     // Connect with WebSocket.
     var host = window.location.hostname;
@@ -27,12 +36,16 @@
 
     // Set data length to 64.
     socket.addEventListener( 'open', function() {
-      socket.send( JSON.stringify( { length: 64 } ) );
+      socket.send( JSON.stringify( { length: config.length } ) );
     });
 
     var row = 0;
     socket.addEventListener( 'message', function( event ) {
       var data = new Float32Array( event.data );
+      if ( data.length !== config.length ) {
+        return;
+      }
+
       drawBackingStore( context.backingStore, data, row++ );
     });
   }
@@ -67,6 +80,82 @@
     }
 
     ctx.putImageData( imageData, x, y );
+  }
+
+  function drawScrubber( ctx, backingCanvas, row ) {
+    var width  = ctx.canvas.width,
+        height = ctx.canvas.height;
+
+    var backingWidth  = backingCanvas.width,
+        backingHeight = backingCanvas.height;
+
+    if ( width > backingWidth || height > backingHeight ) {
+      throw new Error( 'Backing store is smaller than scrubber.' );
+    }
+
+    // Total number of rows.
+    var rowCount = Math.floor( backingWidth / width ) * backingHeight;
+
+    // Initial and final row indices.
+    var ri = Math.max( row, 0 ),
+        rf = ri + height;
+
+    // Move scrubber back up to fit.
+    if ( rf > rowCount ) {
+      ri -= rf - rowCount;
+      rf = rowCount;
+    }
+
+    if ( ri > rf ) {
+      return;
+    }
+
+    var rxi = Math.floor( ri / backingHeight ) * width,
+        ryi = ri % backingHeight;
+
+    var rxf = Math.floor( rf / backingHeight ) * width,
+        ryf = rf % backingHeight;
+
+    ctx.clearRect( 0, 0, width, height );
+
+    if ( rxi !== rxf ) {
+      /**
+       * Scrubber is split across two columns in the backing store:
+       *
+       *    rxi       rxf
+       *     +---------+---------+        ---
+       *     |         | / / / / |         |
+       *     |         |---------+ ryf     |
+       *     |         |         |         |
+       * ryi |---------|         |    backingHeight  ---
+       *     | / / / / |         |         |          |
+       *     |/ / / / /|         |         |     columnHeight
+       *     | / / / / |         |         |          |
+       *     +---------+---------+        ---        ---
+       *
+       *     |- width -|
+       */
+      var columnHeight = backingHeight - ryi;
+
+      ctx.drawImage(
+        backingCanvas,
+        rxi, ryi, width, columnHeight,
+          0,   0, width, columnHeight
+      );
+
+      ctx.drawImage(
+        backingCanvas,
+        rxf,            0, width, ryf,
+          0, columnHeight, width, ryf
+      );
+    } else {
+      // Scrubber is in one column.
+      ctx.drawImage(
+        backingCanvas,
+        rxi, ryi, width, height,
+        0, 0, width, height
+      );
+    }
   }
 
 }) ();
